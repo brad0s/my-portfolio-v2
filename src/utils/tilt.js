34f2bfd01a5de3a46dbf6c6fb2d04feb05c5fa54
Braden Wright <brad0swrizzy@gmail.com>
2023-11-28 15:11:01 -1000

    update tilt for x & y axis

--- src/utils/tilt.js
+++ src/utils/tilt.js
@@ -5,8 +5,10 @@ class TiltController {
     this.imageRect.center = { x: this.imageRect.width / 2, y: this.imageRect.height / 2 };
     this.SCALE_MAX = 5;
     this.SCALE_MIN = -5;
-    this.MAX_DISTANCE = this.getMaxDistance();
-    this.MIN_DISTANCE = this.getMinDistance();
+    this.X_MAX_DISTANCE = this.imageRect.center.x;
+    this.X_MIN_DISTANCE = -this.X_MAX_DISTANCE;
+    this.Y_MAX_DISTANCE = this.imageRect.center.y;
+    this.Y_MIN_DISTANCE = -this.Y_MAX_DISTANCE;
 
     this.root.addEventListener('mousemove', this.handleTilt);
     this.root.addEventListener('mouseleave', this.resetTilt);
@@ -25,8 +27,8 @@ class TiltController {
       y: -(pointer.y - this.imageRect.center.y),
     };
 
-    let scaledX = this.scaleDistance(positionFromCenter.x);
-    let scaledY = this.scaleDistance(positionFromCenter.y);
+    let scaledX = this.XScaleDistance(positionFromCenter.x);
+    let scaledY = this.YScaleDistance(positionFromCenter.y);
 
     // tilt element
     this.root.style.transform = `perspective(1000px) rotateX(${scaledY}deg) rotateY(${scaledX}deg) scale3d(1,1,1)`;
@@ -38,34 +40,20 @@ class TiltController {
   };
 
   // scale the distance to a new range
-  scaleDistance = (distance) => {
+  XScaleDistance = (distance) => {
     const scaledDistance =
-      ((distance - this.MIN_DISTANCE) / (this.MAX_DISTANCE - this.MIN_DISTANCE)) *
+      ((distance - this.X_MIN_DISTANCE) / (this.X_MAX_DISTANCE - this.X_MIN_DISTANCE)) *
         (this.SCALE_MAX - this.SCALE_MIN) +
       this.SCALE_MIN;
     return scaledDistance;
   };
 
-  // get distance from center
-  getDistanceFromCenter = (x2, x1, y2, y1) => {
-    const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-    return distance;
-  };
-
-  // get max distance
-  getMaxDistance = () => {
-    const maxDistance = this.getDistanceFromCenter(
-      this.imageRect.center.x,
-      0,
-      this.imageRect.center.y,
-      0
-    );
-    return Number(maxDistance.toFixed(0));
-  };
-
-  // get min distance
-  getMinDistance = () => {
-    return -Math.abs(this.MAX_DISTANCE);
+  YScaleDistance = (distance) => {
+    const scaledDistance =
+      ((distance - this.Y_MIN_DISTANCE) / (this.Y_MAX_DISTANCE - this.Y_MIN_DISTANCE)) *
+        (this.SCALE_MAX - this.SCALE_MIN) +
+      this.SCALE_MIN;
+    return scaledDistance;
   };
 }
 
